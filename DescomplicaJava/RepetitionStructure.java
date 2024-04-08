f3872267f2337cdb8846391542776062f70b7174
MateusSoliv <mateus2santos.soliv@gmail.com>
2024-04-08 17:13:30 -0300

    Atualizando

--- DescomplicaJava/RepetitionStructure.java
+++ DescomplicaJava/RepetitionStructure.java
@@ -4,7 +4,9 @@ package DescomplicaJava;
 public class RepetitionStructure {
     public static void main(String[] args) {
         
-
+        for(int cont = 8; cont < 17; cont++){
+            System.out.println("Faltam " + cont);
+        }
 
     }
 }
